ed111bf40e592da089b02361a05ba0a5faf86ace
yarenska <silagunes97@gmail.com>
2023-08-12 17:32:44 +0300

    hello

---
+++ src/main/java/com/example/themeparkapplication/ThemeParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.themeparkapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemeParkApplication.class, args);
+    }
+
+}
