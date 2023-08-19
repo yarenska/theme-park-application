2b2facbc63ef844f4bdf149c0c59328edf399cbb
yarenska <silagunes97@gmail.com>
2023-08-19 14:30:15 +0300

    The main classes for the application are written. Test are added

--- src/test/java/com/example/themeparkapplication/ThemeParkApplicationTests.java
+++ src/test/java/com/example/themeparkapplication/ThemeParkApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.themeparkapplication;
+package com.sygunes.themeparkapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ThemeParkApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
